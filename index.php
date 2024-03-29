1cd468ae120f47777ca08cb9a7f8856d1d5569ad
Abdellah Oulahyane <aitbaha48@gmail.com>
2024-03-29 18:40:27 +0300

    --

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 
 <?php
 
-require_once 'vendor/autoload.php';
+require_once './vendor/autoload.php';
 
 
-
-
-//$a = QueryBuilder\App::query()->where('id', '=', 1)->where('name', '=', '234')->where('id', '>=', 122)->orWhere('sdf', 'asdf', 'asdf')->get();
 
 
 QueryBuilder\App::create([
